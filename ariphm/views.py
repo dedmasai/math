d5680fd85ce838510fdbad1f53801d4a3c3333d7
dedmasai <114438951+dedmasai@users.noreply.github.com>
2023-11-17 22:01:35 +0300

    querry b

--- ariphm/views.py
+++ ariphm/views.py
@@ -22,7 +22,7 @@ def quiz(request:HttpRequest):
     else:
 
        # a=[]
-        for task in taskList.objects.all():
+       # for task in taskList.objects.all():
       #      a.append(task.text)
         form=QuizForm
     context ={
