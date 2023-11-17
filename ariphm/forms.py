f6ea3a0ef87e0ec91ef906c4c60e39c7cc3097a5
dedmasai <114438951+dedmasai@users.noreply.github.com>
2023-11-17 23:20:01 +0300

    it form

--- ariphm/forms.py
+++ ariphm/forms.py
@@ -1,11 +1,12 @@
 from django import forms
-from .models import quiz
+from .models import quiz, AnswerQuiz
 class QuizForm(forms.Form):
     name = forms.CharField()
     a1= forms.IntegerField()
     a2 = forms.IntegerField()
     a3 = forms.IntegerField()
 
-#class QuizItForm(forms.ModelForm):
- #   class Meta
-  #     fields = "__all__"
+class QuizItForm(forms.ModelForm):
+    class Meta:
+        model=AnswerQuiz
+        fields = "__all__"
