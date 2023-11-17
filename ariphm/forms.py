cfd08e548dc65b6dd09519bfa8e876e5e640851c
dedmasai <114438951+dedmasai@users.noreply.github.com>
2023-11-17 22:55:33 +0300

    QuizItForm

--- ariphm/forms.py
+++ ariphm/forms.py
@@ -6,7 +6,7 @@ class QuizForm(forms.Form):
     a2 = forms.IntegerField()
     a3 = forms.IntegerField()
 
-class QuizItForm(forms.ModelForm):
-    class Meta
-        model = quiz
-        fields = ['uAnswer']
+#class QuizItForm(forms.ModelForm):
+#    class Meta
+ #       model = quiz
+  #      fields = ['uAnswer']
