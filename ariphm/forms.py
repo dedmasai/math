7a70f64fda99176b6f3fcd85e2d3f15157fba0dc
dedmasai <114438951+dedmasai@users.noreply.github.com>
2023-11-17 22:51:36 +0300

    QuizItForm

--- ariphm/forms.py
+++ ariphm/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-
+from .models import quiz
 class QuizForm(forms.Form):
     name = forms.CharField()
     a1= forms.IntegerField()
     a2 = forms.IntegerField()
     a3 = forms.IntegerField()
+
+class QuizItForm(forms.ModelForm):
+    class Meta
+        model = quiz
+        fields = ['answer']
