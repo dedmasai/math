4cbbe296c9b6c8fc0ae516cfab970c8697fa9c21
dedmasai <114438951+dedmasai@users.noreply.github.com>
2023-11-17 17:01:56 +0300

    task back

--- ariphm/models.py
+++ ariphm/models.py
@@ -8,10 +8,9 @@ class Variant(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
 
 class Task(models.Model):
-    themeNumber=models.IntegerField(default=0)
-    tText=models.TextField(null=False, blank=True)
-    tAnswer=models.IntegerField(default=0)
-    tMod=models.DateTimeField(auto_now_add=True)
+    name=models.CharField(max_length=100)
+    description=models.TextField(null=False, blank=True)
+    answer=models.IntegerField(default=0)
     variants=models.ManyToManyField(Variant,related_name="tasks")
 
     def __str__(self):
