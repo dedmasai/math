e973e5f5c1c00d66b5102f8ae036bd8d98f1bfd0
dedmasai <114438951+dedmasai@users.noreply.github.com>
2023-11-18 11:47:10 +0300

    var

--- ariphm/views.py
+++ ariphm/views.py
@@ -24,7 +24,7 @@ def quiz(request:HttpRequest):
 
         form=QuizItForm
     context ={
-        "var":var
+      #  "var":var
         "form":form
     }
     return render(request,"ariphm/quiz.html", context=context)
